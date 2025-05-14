e2f41c63d21fdf731a0fb422cd648558595d34d9
nefriston <zelenskikhali@gmail.com>
2025-05-14 13:00:59 +0300

    try this

---
+++ test-things/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let const PI: f64 = 3.14159265358979323846;
+    let mut PI=0.0;
+    println!(PI);
+}
---
+++ test-things/target/debug/deps/test_things-767c1767e897f8bc.d
@@ -0,0 +1,5 @@
+C:\Users\Ali Zelenskikh\Documents\поделки\rust поделки\rust-things\test-things\target\debug\deps\libtest_things-767c1767e897f8bc.rmeta: src\main.rs
+
+C:\Users\Ali Zelenskikh\Documents\поделки\rust поделки\rust-things\test-things\target\debug\deps\test_things-767c1767e897f8bc.d: src\main.rs
+
+src\main.rs:
